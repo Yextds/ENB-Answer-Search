1bdf497725a31b9fe1a8a9edb3146ff219dac943
sameer <sameer.khan@dotsquares.com>
2023-06-16 12:21:03 +0530

    add website schema

--- src/templates/LocationsPage.tsx
+++ src/templates/LocationsPage.tsx
@@ -12,21 +12,21 @@ import "../index.css";
 import {
   SearchHeadlessProvider,
   provideHeadless,
-  
 } from "@yext/search-headless-react";
 import { JsonLd } from "react-schemaorg";
 import {
   SearchBar,
-  
   SpellCheck,
   ResultsCount,
-  
   AppliedFilters,
   LocationBias,
 } from "@yext/search-ui-react";
 import DirectAnswer from "../components/DirectAnswer";
 import Navigation from "../components/Navigation";
-import { answersHeadlessConfig, baseUrl } from "../config/answersHeadlessConfig";
+import {
+  answersHeadlessConfig,
+  baseUrl,
+} from "../config/answersHeadlessConfig";
 
 import Footer from "../components/commons/Footer";
 import { LocationCard } from "../components/cards/LocationCard";
@@ -37,6 +37,7 @@ import ENBPolygonLogo from "../Images/Answer-Head-logo.svg";
 import NewPagination from "../components/commons/PaginationComponent";
 import favicon from "../Images/favicon.png";
 import HeaderLogo from "../components/commons/HeaderLogo";
+import { BreadcrumbList, WebSite } from "schema-dts";
 
 export const config: TemplateConfig = {
   stream: {
@@ -59,11 +60,12 @@ export const config: TemplateConfig = {
 export const getPath: GetPath<TemplateProps> = () => {
   return "/locations";
 };
-const metaTitle = "Ephrata National Bank | Locations"
-const metaDescription = "Ephrata National Bank | Find all branch details, ATMs timings and Locations here."
-export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
-  
-}): HeadConfig => {
+const metaTitle = "Ephrata National Bank | Locations";
+const metaDescription =
+  "Ephrata National Bank | Find all branch details, ATMs timings and Locations here.";
+export const getHeadConfig: GetHeadConfig<
+  TemplateRenderProps
+> = ({}): HeadConfig => {
   return {
     title: `Ephrata National Bank | AS`,
     charset: "UTF-8",
@@ -72,114 +74,113 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
       {
         type: "link",
         attributes: {
-            rel: "icon",
-            type: "image/x-icon",
-            href: favicon,
+          rel: "icon",
+          type: "image/x-icon",
+          href: favicon,
         },
-    },
-  
-   {
-     type: "meta",
-     attributes: {
-       name: "title",
-       content: `Ephrata National Bank | Locations`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       name: "description",
-       content: `Ephrata National Bank | Find all branch details, ATMs timings and Locations here.`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       name: "author",
-       content: "Ephrata National Bank",
-     },
-   },
-   {
-     type: "link",
-     attributes: {
-       rel: "canonical",
-       href: baseUrl + "locations",
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       property: "og:title",
-       content: `${metaTitle}`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       property: "og:description",
-       content: `${metaDescription}`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       property: "og:url",
-       content: baseUrl,
-     },
-   },
-  
-   {
-     type: "meta",
-     attributes: {
-       property: "og:image",
-       content: `${ENBRoundLogo}`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       property: "twitter:title",
-       content: `${metaTitle}`,
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       name: "twitter:card",
-       content: "summary",
-     },
-   },
-   {
-     type: "meta",
-     attributes: {
-       name: "twitter:url",
-       content: baseUrl,
-     },
-   },
-  
-   {
-     type: "meta",
-     attributes: {
-       name: "twitter:description",
-       content: `${metaDescription}`,
-     },
-   },
-  
-   {
-     type: "meta",
-     attributes: {
-       name: "twitter:image",
-       content: `${ENBRoundLogo}`,
-     },
-   },
-   {
-    type: "meta",
-    attributes: {
-      name: "robots",
-      content: "noindex, nofollow",
-    },
-  },
-    
+      },
+
+      {
+        type: "meta",
+        attributes: {
+          name: "title",
+          content: `Ephrata National Bank | Locations`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          name: "description",
+          content: `Ephrata National Bank | Find all branch details, ATMs timings and Locations here.`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          name: "author",
+          content: "Ephrata National Bank",
+        },
+      },
+      {
+        type: "link",
+        attributes: {
+          rel: "canonical",
+          href: baseUrl + "locations",
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          property: "og:title",
+          content: `${metaTitle}`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          property: "og:description",
+          content: `${metaDescription}`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          property: "og:url",
+          content: baseUrl,
+        },
+      },
+
+      {
+        type: "meta",
+        attributes: {
+          property: "og:image",
+          content: `${ENBRoundLogo}`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          property: "twitter:title",
+          content: `${metaTitle}`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          name: "twitter:card",
+          content: "summary",
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          name: "twitter:url",
+          content: baseUrl,
+        },
+      },
+
+      {
+        type: "meta",
+        attributes: {
+          name: "twitter:description",
+          content: `${metaDescription}`,
+        },
+      },
+
+      {
+        type: "meta",
+        attributes: {
+          name: "twitter:image",
+          content: `${ENBRoundLogo}`,
+        },
+      },
+      {
+        type: "meta",
+        attributes: {
+          name: "robots",
+          content: "noindex, nofollow",
+        },
+      },
     ],
   };
 };
@@ -187,44 +188,47 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
 answersHeadlessConfig.verticalKey = "locations";
 
 const searcher = provideHeadless(answersHeadlessConfig);
-interface LocationType {
-  "@type": "BreadcrumbList";
-  name: string;
-  url: string;
-  logo: string;
-}
 
-const LocationsPage: Template<TemplateRenderProps> = ({
- 
-  document,
-}) => {
+const LocationsPage: Template<TemplateRenderProps> = ({ document }) => {
   const { _site } = document;
-  
 
   return (
     <>
-    <JsonLd<LocationType>
+      <JsonLd<BreadcrumbList>
         item={{
           "@context": "https://schema.org",
           "@type": "BreadcrumbList",
           name: "Ephrata National Bank",
           url: "https://www.epnb.com/answers/",
-          logo: ENBRoundLogo,
         }}
       />
-     
+      <JsonLd<WebSite>
+        item={{
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          "url": "https://www.epnb.com/",
+          "name": "Ephrata National Bank",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": {
+              "@type": "EntryPoint",
+              "urlTemplate": "https://www.epnb.com/search/{search_term_string}/",
+            },
+          }
+        }}
+      />
       <SearchHeadlessProvider searcher={searcher}>
         <LocationProvider>
           <div className="container-custom px-5 py-4 xs:py-[1.875rem]">
             {/* Round Centered Logo */}
-           <HeaderLogo/>
+            <HeaderLogo />
             {/* Round Centered Logo */}
           </div>
           <div className="py-4 xs:pb-14 pt-0">
             <div className="container-custom px-5 pb-4 xs:pb-8 pt-0 xs:px-[4.375rem] bg-white rounded">
               <div className="polygon-logo mb-10">
                 <div className="polygon-centred">
-                  <img className="mx-auto" src={ENBPolygonLogo} alt="logo"/>
+                  <img className="mx-auto" src={ENBPolygonLogo} alt="logo" />
                 </div>
               </div>
               <div className="yext-search-bar">
@@ -246,7 +250,14 @@ const LocationsPage: Template<TemplateRenderProps> = ({
           </div>
         </LocationProvider>
       </SearchHeadlessProvider>
-      <Footer footerHeading={_site.c_footerHeading} footerlinks={_site.c_footerlinks} CopyrightText={_site.c_copyrightText} FooterLabel={_site.c_footerLabel} FooterAddress={_site.c_footerAddress} number={_site.c_number}/>
+      <Footer
+        footerHeading={_site.c_footerHeading}
+        footerlinks={_site.c_footerlinks}
+        CopyrightText={_site.c_copyrightText}
+        FooterLabel={_site.c_footerLabel}
+        FooterAddress={_site.c_footerAddress}
+        number={_site.c_number}
+      />
     </>
   );
 };
